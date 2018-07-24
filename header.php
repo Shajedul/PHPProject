f41cf086c58c24ed0ca85c41a2f499b139896f14
Shajedul Islam Shuvo <helloitsshuvo@gmail.com>
2018-07-25 05:36:22 +0600

    Initial commit

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Sample form for php</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<header>
+    <nav>
+        <div class="main-wrapper">
+            <ul>
+                <li>
+                    <a href="index.php">Home</a>
+                </li>
+            </ul>
+            <div class="nav-login">
+                <form>
+                    <input type="text" name="uid" placeholder="User Name/ Email">
+                    <input type="password" name="pass" placeholder="Password">
+                    <button type="submit" name="submit">Log In</button>
+                </form>
+                <a href="signup.php">Sign Up</a>
+            </div>
+
+        </div>
+    </nav>
+</header>
---
+++ signup.php
@@ -0,0 +1,22 @@
+<?php
+include_once 'header.php';
+?>
+<section class="main-container">
+    <div class="main-wrapper">
+        <h2>Sign Up Here</h2>
+
+        <form class="signup-form">
+            <input type="text" name="first" placeholder="First Name">
+            <input type="text" name="last" placeholder="Last Name">
+            <input type="text" name="email" placeholder="Email">
+            <input type="text" name="uid" placeholder="User Name">
+            <input type="password" name="pwd" placeholder="Password">
+            <button type="submit" name="signup">Sign Up</button>
+
+        </form>
+
+    </div>
+</section>
+<?php
+include_once 'footer.php';
+?>
